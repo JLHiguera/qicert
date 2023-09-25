f8d7b8e881377ee49a9b2931b1e6f0ba121134bd
José Luis A. Higuera <jose@higuera.dev>
2023-09-25 03:00:42 -0600

    Tests created for domain_name struct

--- src/domain/domain_name.rs
+++ src/domain/domain_name.rs
@@ -1,13 +1,11 @@
-use std::{fmt::Display, str::FromStr, ops::Deref};
+use std::{fmt::Display, str::FromStr};
 
 use super::DomainError;
-
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct DomainName(String);
 
 impl DomainName {
-
     pub fn get_name(&self) -> String {
         self.0.clone()
     }
@@ -36,7 +34,6 @@ impl Display for DomainName {
 }
 
 impl FromStr for DomainName {
-    //type Err = DomainError;
     type Err = DomainError;
     
     fn from_str(value: &str) -> Result<Self, Self::Err> {
@@ -50,3 +47,50 @@ impl FromStr for DomainName {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn valid_domain_name_from_str() {
+        let names = vec![
+            ("example", true),
+            (".", false),
+            ("", false),
+            ("example.", false),
+            ("example.net", false),
+            ("university", true),
+            ("example123", true),
+            ("example ", false),
+            ("still-valid", true),
+            ("-not-valid", false),
+            ("not-valid-either-", false),
+            ("not_valid", false),
+        ];
+
+        for (name, expected) in names {
+            let domain_name = DomainName::from_str(name);
+
+            assert_eq!(domain_name.is_ok(), expected);
+        }
+    }
+
+    #[test]
+    fn domain_matches_expected_str() {
+        let names = vec![
+            ("EXAMPLE", "example"),
+            ("EXamPLE", "example"),
+            ("EXAM123ple", "exam123ple"),
+        ];
+
+        for (name, expected) in names {
+            let domain_name = DomainName::from_str(name);
+
+            assert!(domain_name.is_ok());
+
+            if let Ok(domain_name) = domain_name {
+                assert_eq!(domain_name.to_string(), expected);
+            }
+        }
+    }
+}
